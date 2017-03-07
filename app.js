e103178c4a31d5f927cde1eaff71ef6e62657298
saurabhketech <shekhar>
2017-03-07 16:01:53 +0530

    remove useless codes

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
 var express = require('express'); //require express module
 var app = express(); //creatig insatnce of express function
 var mongoose = require('mongoose'); //require moongose module
-var bodyParser = require('body-parser');
 var db = require('./mongodb/db.js'); // create route for database
 var routes = require('./routes/index.js'); //create route for index
-var crypto = require('crypto');
-var moment = require("moment");
 var cors = require('cors');
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true })); //urlencoded within bodyParsar , extract data from <form> element
-app.use(bodyParser.json());
 app.use(db());
 app.use('/', routes);
 app.use(errorHandler);
